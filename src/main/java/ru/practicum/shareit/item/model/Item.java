da72c2cf6ff2be6d42bcc0a850d20a7767c18941
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-07-08 10:17:53 +0300

    edit #2

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,13 +1,16 @@
 package ru.practicum.shareit.item.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Data
 @Builder(toBuilder = true)
 public class Item {
+
     private Long id;
     private String name;
     private String description;
--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
 @Data
 @Builder(toBuilder = true)
 public class User {
