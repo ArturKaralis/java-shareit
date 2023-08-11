330dcc0650a21d2a24bf82751afd0c383f789e42
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-08-11 15:09:10 +0300

    first

--- src/main/java/ru/practicum/shareit/exception/MethodArgumentException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class MethodArgumentException extends RuntimeException {
-    public MethodArgumentException(String message) {
-        super(message);
-    }
-}
